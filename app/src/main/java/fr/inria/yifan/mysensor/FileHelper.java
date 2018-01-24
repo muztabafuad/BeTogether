b6a18b719ce1fc6d0b0b19546f5b7f4d8e5c5763
yifan-du <yifan.du.14@gmail.com>
2018-01-24 21:36:49 +0100

    Start sensing, stop sensing and storing file.

---
+++ app/src/main/java/fr/inria/yifan/mysensor/FileHelper.java
@@ -0,0 +1,79 @@
+package fr.inria.yifan.mysensor;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.support.v4.app.ActivityCompat.requestPermissions;
+
+/**
+ * This class provides functions including storing and reading sensing data file.
+ */
+
+public class FileHelper {
+
+    private static final String TAG = "File storage";
+
+    // Declare file storage permissions
+    private static final int PERMS_REQUEST_STORAGE = 1000;
+    @SuppressLint("InlinedApi")
+    private static final String[] STORAGE_PERMS = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private Context context;
+
+    FileHelper(Context context) {
+        super();
+        this.context = context;
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context, "Requesting storage permission", Toast.LENGTH_SHORT).show();
+            // Request permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions((Activity) context, STORAGE_PERMS, PERMS_REQUEST_STORAGE);
+            } else {
+                Toast.makeText(context, "Please give storage permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Write file to storage
+    void savaFile(String filename, String filecontent) throws Exception {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            filename = Environment.getExternalStorageDirectory().getCanonicalPath() + "/Documents/" + filename;
+            //Log.d(TAG, filename);
+            FileOutputStream output = new FileOutputStream(filename);
+            output.write(filecontent.getBytes());
+            output.close();
+        } else {
+            Toast.makeText(context, "Failed in writing file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Read file from storage
+    public String readFile(String filename) throws IOException {
+        StringBuilder sb = new StringBuilder("");
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            filename = Environment.getExternalStorageDirectory().getCanonicalPath() + "/Documents/" + filename;
+            FileInputStream input = new FileInputStream(filename);
+            byte[] temp = new byte[1024];
+            int len;
+            while ((len = input.read(temp)) > 0) {
+                sb.append(new String(temp, 0, len));
+            }
+            input.close();
+        } else {
+            Toast.makeText(context, "Failed in writing file", Toast.LENGTH_SHORT).show();
+        }
+        return sb.toString();
+    }
+}
--- app/src/main/java/fr/inria/yifan/mysensor/SoundActivity.java
+++ app/src/main/java/fr/inria/yifan/mysensor/SoundActivity.java
@@ -7,6 +7,8 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -27,10 +29,15 @@ public class SoundActivity extends AppCompatActivity {
     // Audio recorder parameters for sampling
     private static final int SAMPLE_RATE_IN_HZ = 8000;
     private final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE_IN_HZ, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
+
     // Thread locker and running flag
     private final Object mLock;
     private AudioRecord mAudioRecord;
     private boolean isGetVoiceRun;
+
+    // File helper and string data
+    private FileHelper fileHelper;
+    private StringBuilder stringBuilder;
 
     // Constructor initializes locker
     public SoundActivity() {
@@ -42,11 +49,20 @@ public class SoundActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
-        senseSound();
+        fileHelper = new FileHelper(this);
+        checkPermission();
+        startRecord();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                stopRecord();
+            }
+        }, 10000);
     }
 
     // Start to sense the sound
-    private void senseSound() {
+    private void checkPermission() {
         // Check user permission for microphone
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Requesting Microphone permission", Toast.LENGTH_SHORT).show();
@@ -57,39 +73,28 @@ public class SoundActivity extends AppCompatActivity {
                 Toast.makeText(this, "Please give Microphone permission", Toast.LENGTH_SHORT).show();
                 finish();
             }
-        } else {
-            getNoiseLevel();
         }
     }
 
     // Callback for user allowing permission
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
             case PERMS_REQUEST_RECORD: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getNoiseLevel();
-                } else {
+                if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "Please give Microphone permission", Toast.LENGTH_SHORT).show();
-                    finish();
+                    checkPermission();
                 }
             }
         }
     }
 
     private void startRecord() {
-        //这里我们使用私有模式,创建出来的文件只能被本应用访问,还会覆盖原文件哦
-        //FileOutputStream output = this.openFileOutput(filename, this.MODE_PRIVATE);
-        //output.write(filecontent.getBytes());
-        //output.close();
-    }
-
-    // Get the sound level
-    private void getNoiseLevel() {
         if (isGetVoiceRun) {
             Log.e(TAG, "Still in recording");
             return;
         }
+        stringBuilder = new StringBuilder();
         mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE_IN_HZ, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT, BUFFER_SIZE);
         isGetVoiceRun = true;
         new Thread(new Runnable() {
@@ -109,6 +114,7 @@ public class SoundActivity extends AppCompatActivity {
                     double mean = v / (double) r;
                     double volume = 10 * Math.log10(mean);
                     Log.d(TAG, "Sound dB value: " + volume);
+                    stringBuilder.append(System.currentTimeMillis()).append(", ").append(volume).append("\n");
                     // 10 times per second
                     synchronized (mLock) {
                         try {
@@ -124,4 +130,17 @@ public class SoundActivity extends AppCompatActivity {
             }
         }).start();
     }
+
+    // Get the sound level
+    private void stopRecord() {
+        isGetVoiceRun = false;
+        String time = String.valueOf(System.currentTimeMillis());
+        //Log.d(TAG, "Now is " + time);
+        try {
+            fileHelper.savaFile(time, stringBuilder.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
