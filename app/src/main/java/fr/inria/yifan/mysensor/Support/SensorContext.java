37b0f7e2572d062010eab63f611dd09c035907a4
Yifan-DU <yifan.du.14@gmail.com>
2018-02-06 17:00:55 +0100

    Battery determination initialized.

--- app/src/main/java/fr/inria/yifan/mysensor/Support/SensorContext.java
+++ app/src/main/java/fr/inria/yifan/mysensor/Support/SensorContext.java
@@ -1,6 +1,9 @@
 package fr.inria.yifan.mysensor.Support;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.ArrayMap;
@@ -24,6 +27,21 @@ public class SensorContext {
     public SensorContext(Activity activity) {
         mActivity = activity;
         //sensorArray = new ArrayMap<>();
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = mActivity.registerReceiver(null, filter);
+
+        // Are we charging / charged?
+        assert batteryStatus != null;
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = (status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL);
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean acCharge = (chargePlug == BatteryManager.BATTERY_PLUGGED_AC);
+
+        // Get the current battery capacity
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        float batteryPct = level / (float) scale;
     }
 
 }
