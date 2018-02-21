0674d47d0aa3cb0f501562c8ede29e9e58b0ab1b
yifan-du <yifan.du.14@gmail.com>
2018-02-21 20:56:41 +0100

    Start to merge Wifi P2P helper to Wifi Direct helper.

--- app/src/main/java/fr/inria/yifan/mysensor/Support/WifiDirectHelper.java
+++ app/src/main/java/fr/inria/yifan/mysensor/Support/WifiDirectHelper.java
@@ -1,11 +1,15 @@
 package fr.inria.yifan.mysensor.Support;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
@@ -16,6 +20,7 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,7 +30,7 @@ import static fr.inria.yifan.mysensor.Support.Configuration.ENABLE_REQUEST_WIFI;
  * This class provides functions functions related to the Wifi Direct service..
  */
 
-public class WifiDirectHelper {
+public class WifiDirectHelper extends BroadcastReceiver {
 
     private static final String TAG = "Wifi Direct helper";
 
@@ -37,6 +42,23 @@ public class WifiDirectHelper {
     // Declare channel and Wifi Direct manager
     private WifiP2pManager.Channel mChannel;
     private WifiP2pManager mManager;
+    private WifiP2pManager.ConnectionInfoListener mConnectionListener = new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
+            // Inet Address from WifiP2pInfo struct.
+            InetAddress groupOwnerAddress = wifiP2pInfo.groupOwnerAddress;
+            // After the group negotiation, we can determine the group owner (server).
+            if (wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner) {
+                // Do whatever tasks are specific to the group owner.
+                // One common case is creating a group owner thread and accepting incoming connections.
+                Toast.makeText(mActivity, "I am the grouper owner.", Toast.LENGTH_LONG).show();
+            } else if (wifiP2pInfo.groupFormed) {
+                // The other device acts as the peer (client).
+                // In this case, you'll want to create a peer thread that connects to the group owner.
+                Toast.makeText(mActivity, "I am the grouper member of " + groupOwnerAddress, Toast.LENGTH_LONG).show();
+            }
+        }
+    };
 
     // Constructor
     public WifiDirectHelper(Activity activity) {
@@ -171,4 +193,48 @@ public class WifiDirectHelper {
         });
     }
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        assert action != null;
+        switch (action) {
+            case WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION:
+                // Check to see if Wi-Fi is enabled and notify appropriate activity
+                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+                switch (state) {
+                    case WifiP2pManager.WIFI_P2P_STATE_ENABLED:
+                        // Wifi P2P is enabled
+                        break;
+                    default:
+                        // Wi-Fi P2P is not enabled
+                        break;
+                }
+                break;
+            case WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION:
+                // Call WifiP2pManager.requestPeers() to get a list of current peers
+                // request available peers from the wifi p2p manager. This is an
+                // asynchronous call and the calling activity is notified with a
+                // callback on PeerListListener.onPeersAvailable()
+                mManager.requestPeers(mChannel, new WifiP2pManager.PeerListListener() {
+                    // Declare Wifi P2P peer list listener
+                    @Override
+                    public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                        mAdapterWifi.clear();
+                        mAdapterWifi.addAll(wifiP2pDeviceList.getDeviceList());
+                    }
+                });
+                break;
+            case WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION:
+                // Respond to new connection or disconnections
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+                if (networkInfo.isConnected()) {
+                    // We are connected with the other device, request connection info to find group owner IP
+                    mManager.requestConnectionInfo(mChannel, mConnectionListener);
+                }
+                break;
+            case WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION:
+                // Respond to this device's wifi state changing
+                break;
+        }
+    }
 }
