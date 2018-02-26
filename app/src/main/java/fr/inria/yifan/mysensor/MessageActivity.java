4c81020b4ddedabfbde70fec4ce44eab540b61df
Yifan-DU <yifan.du.14@gmail.com>
2018-02-26 17:33:33 +0100

    Initialized a message activity.

---
+++ app/src/main/java/fr/inria/yifan/mysensor/MessageActivity.java
@@ -0,0 +1,10 @@
+package fr.inria.yifan.mysensor;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * This class provides a UI for message exchange.
+ */
+
+public class MessageActivity extends AppCompatActivity {
+}
