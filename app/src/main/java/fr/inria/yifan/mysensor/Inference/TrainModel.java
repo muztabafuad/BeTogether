18b373c0d85d713c4e51740993e784d0ebef4a21
Yifan-DU <yifan.du.14@gmail.com>
2018-06-08 15:33:45 +0200

    Evaluation on incremental learning.

--- app/src/main/java/fr/inria/yifan/mysensor/Inference/TrainModel.java
+++ app/src/main/java/fr/inria/yifan/mysensor/Inference/TrainModel.java
@@ -20,49 +20,50 @@ public class TrainModel {
         try {
 
             // Load data from csv file
-            DataSource source_train = new DataSource("/Users/yifan/OneDrive/INRIA/Context Sense/Training Data/GT-I9505_1527175592592.csv");
-            DataSource source_test = new DataSource("/Users/yifan/OneDrive/INRIA/Context Sense/Training Data/Redmi Note 4_1527233438107.csv");
+            DataSource source_train = new DataSource("/Users/yifan/OneDrive/INRIA/Context Sense/Training Data/GT-I9505.csv");
+            DataSource source_test = new DataSource("/Users/yifan/OneDrive/INRIA/Context Sense/Training Data/Redmi-Note4.csv");
             Instances train = source_train.getDataSet();
             Instances test = source_test.getDataSet();
 
             /*
-            1 timestamp 2 daytime (b) 3 light density (lx) 4 magnetic strength (μT) 5 GSM active (b)
-            6 RSSI level 7 GPS accuracy (m) 8 Wifi active (b) 9 Wifi RSSI (dBm) 10 proximity (b)
-            11 sound level (dBA) 12 temperature (C) 13 pressure (hPa) 14 humidity (%)
-            15 in-pocket label 16 in-door label 17 under-ground label
+            1 timestamp, 2 daytime (b), 3 light density (lx), 4 magnetic strength (μT), 5 GSM active (b),
+            6 RSSI level, 7 RSSI value (dBm), 8 GPS accuracy (m), 9 Wifi active (b), 10 Wifi RSSI (dBm),
+            11 proximity (b), 12 sound level (dBA), 13 temperature (C), 14 pressure (hPa), 15 humidity (%),
+            16 in-pocket label, 17 in-door label, 18 under-ground label"
             */
 
-            // Remove unused attributes
+            // Only keep used attributes
             Remove remove = new Remove();
-            remove.setAttributeIndices("3, 10, 15");
+            remove.setAttributeIndices("6, 8, 13, 9, 7, 14, 10, 15, 18");
             remove.setInvertSelection(true);
             remove.setInputFormat(train);
-
             Instances revTrain = Filter.useFilter(train, remove);
             Instances revTest = Filter.useFilter(test, remove);
 
             // Convert class to nominal
             NumericToNominal nominal = new NumericToNominal();
-            nominal.setAttributeIndices("3");
+            nominal.setAttributeIndices(String.valueOf(revTrain.numAttributes()));
             nominal.setInputFormat(revTrain);
-
             Instances newTrain = Filter.useFilter(revTrain, nominal);
             Instances newTest = Filter.useFilter(revTest, nominal);
 
             // Set data class label index
             newTrain.setClassIndex(newTrain.numAttributes() - 1);
             newTest.setClassIndex(newTest.numAttributes() - 1);
+            newTrain.randomize(new Random());
             newTest.randomize(new Random());
 
-            for (int i = 0; i < newTrain.numAttributes(); i++) {
+            // Show all attributes
+            for (int i = 0; i < newTrain.numAttributes() - 1; i++) {
                 System.out.print(newTrain.attribute(i).name());
             }
+            System.out.print(" Target:" + newTrain.classAttribute().name());
 
             // 10-fold cross validation
-            HoeffdingTree tree = new HoeffdingTree(); // new instance of tree
-            Evaluation cv = new Evaluation(newTrain);
-            cv.crossValidateModel(tree, newTrain, 10, new Random(1));
-            System.out.println(cv.toSummaryString());
+            HoeffdingTree classifier = new HoeffdingTree();
+            Evaluation cross = new Evaluation(newTrain);
+            cross.crossValidateModel(classifier, newTrain, 10, new Random());
+            System.out.println(cross.toSummaryString());
 
             long startTime;
             long endTime;
@@ -70,44 +71,47 @@ public class TrainModel {
 
             // Batch training
             startTime = System.nanoTime();
-            tree.buildClassifier(newTrain);   // build classifier
+            classifier.buildClassifier(newTrain);   // build classifier
             endTime = System.nanoTime();
-            System.out.println("Training time (batch): " + (endTime - startTime));
+            System.out.println("Training time (in batch): " + (endTime - startTime));
 
             // Evaluate classifier on data set
             Evaluation eval = new Evaluation(newTest);
-            eval.evaluateModel(tree, newTest);
+            eval.evaluateModel(classifier, newTest);
             System.out.println(eval.toSummaryString());
 
             // Incremental training
-            int boostRun = 1000;
+            int boost = 1;
+            int counter = 0;
             totalTime = 0;
-            for (int i = 0; i < newTest.numInstances(); i++) {
-                startTime = System.nanoTime();
-                // Boosting the incrementL learning
-                for (int j = 0; j < boostRun; j++){
-                    tree.updateClassifier(newTest.instance(i));
-                }
-                endTime = System.nanoTime();
-                totalTime += endTime - startTime;
+            Random random = new Random();
+            for (int i = 0; i < 10000; i++) {
                 Evaluation ev = new Evaluation(newTest);
-                ev.evaluateModel(tree, newTest);
-                System.out.println("Iteration: " + i + " Accuracy: " + ev.pctCorrect());
+                ev.evaluateModel(classifier, newTest);
+                // Opportunistic feedback on wrong inference
+                if (random.nextInt(100) > ev.pctCorrect()) {
+                    startTime = System.nanoTime();
+                    for (int j = 0; j < boost; j++) {
+                        classifier.updateClassifier(newTest.instance(i));
+                    }
+                    endTime = System.nanoTime();
+                    totalTime += endTime - startTime;
+                    counter ++;
+                    System.out.println("Feedback number: " + counter + " Accuracy: " + ev.pctCorrect());
+                }
             }
-            System.out.println("Training time (mean, nano): " + totalTime / newTest.numInstances());
+            System.out.println("Training time (incremental): " + totalTime / counter);
 
             // Save and load
-            SerializationHelper.write("/Users/yifan/Documents/MySensor/app/src/main/assets/HoeffdingTree.model", tree);
-            tree = (HoeffdingTree) SerializationHelper.read("/Users/yifan/Documents/MySensor/app/src/main/assets/Tree.model");
+            SerializationHelper.write("/Users/yifan/Documents/MySensor/app/src/main/assets/Classifier.model", classifier);
+            classifier = (HoeffdingTree) SerializationHelper.read("/Users/yifan/Documents/MySensor/app/src/main/assets/Classifier.model");
 
             // Classify new instance
             Instances dataSet = new Instances(newTrain, 0);
-            Instance inst = new DenseInstance(3);
-            inst.setValue(0, 200.0f);
-            inst.setValue(1, 8.0f);
+            Instance inst = new DenseInstance(1, new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0});
             inst.setDataset(dataSet);
-            int result = (int) tree.classifyInstance(inst);
-            //System.out.println("Sample: " + inst + ", Inference: " + result);
+            int result = (int) classifier.classifyInstance(inst);
+            System.out.println("Sample: " + inst + ", Inference: " + result);
 
         } catch (Exception e) {
             e.printStackTrace();
